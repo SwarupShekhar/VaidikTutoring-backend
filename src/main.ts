26aee49baae5f9d7de15500a262c6bac0599e732
Swarup Shekhar <swarupshekhar@Mac.lan>
2026-02-02 17:00:53 +0530

    FIxed deployment error

--- src/main.ts
+++ src/main.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`🚀 Application is running on: http://localhost:${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
