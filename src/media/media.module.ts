798249431d599e164d18c539b807debdf9b0e456
Swarup Shekhar <shekharswarup67@gmail.com>
2026-03-13 14:26:11 +0530

    Fix MediaModule dependency issues for JwtAuthGuard

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MediaController } from './media.controller';
 import { StorageModule } from '../storage/storage.module';
+import { AuthModule } from '../auth/auth.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-    imports: [StorageModule],
+    imports: [StorageModule, AuthModule, PrismaModule],
     controllers: [MediaController],
 })
 export class MediaModule { }
