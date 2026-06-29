40e70c62d0a7b2478d452b888f4818cd7eeb2c1c
Swarup Shekhar <shekharswarup67@gmail.com>
2026-06-29 15:21:17 +0530

    Fix admin module dependency on ZoomService

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -7,6 +7,7 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { EmailModule } from '../email/email.module';
 import { AzureModule } from '../azure/azure.module';
 import { LeadsModule } from '../leads/leads.module';
+import { ZoomModule } from '../zoom/zoom.module';
 
 @Global()
 @Module({
@@ -15,6 +16,7 @@ import { LeadsModule } from '../leads/leads.module';
     EmailModule,
     AzureModule,
     LeadsModule,
+    ZoomModule,
   ],
   controllers: [AdminController, SeedPricingController],
   providers: [AdminService, SyncClerkMetadataService],
